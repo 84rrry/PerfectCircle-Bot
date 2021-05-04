7e51dbb7fe4d9d3901c7daf86c33ff79158ad597
84rrry <83638436+84rrry@users.noreply.github.com>
2021-05-04 09:45:47 +0100

    Update PerfectCircle .py

--- PerfectCircle .py
+++ PerfectCircle .py
@@ -1,12 +1,19 @@
 import pyautogui,math
+#Drag mouse to top left corner to stop the automation 
 pyautogui.FAILSAFE=True
+#define radius
 Radius = 250
+#define speed
 Speed=8
+#to change the window
 pyautogui.hotkey('alt', 'tab')
+#locate the center 
 (X,Y)=pyautogui.locateCenterOnScreen('circle.png')
 X+=1
 Y+=1
+#move to the right initial position
 pyautogui.moveTo(X+Radius,Y)
+#draw the circle 
 pyautogui.mouseDown()
 for i in range(370):
     if i%Speed==0:
